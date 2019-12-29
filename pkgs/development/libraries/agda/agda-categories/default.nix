2595a55e8e282e72c1521804b8cfb75be25c8ea5
Alex Rice <alexrice999@hotmail.co.uk>
2019-12-29 17:22:00 +0000

    agda-categories: init at 0.1

---
+++ pkgs/development/libraries/agda/agda-categories/default.nix
@@ -0,0 +1,24 @@
+{ lib, mkDerivation, fetchFromGitHub, standard-library }:
+
+mkDerivation rec {
+  version = "0.1";
+  pname = "agda-categories";
+
+  src = fetchFromGitHub {
+    owner = "agda";
+    repo = "agda-categories";
+    rev = "release/v${version}";
+    sha256 = "0m4pjy92jg6zfziyv0bxv5if03g8k4413ld8c3ii2xa8bzfn04m2";
+  };
+
+  buildInputsAgda = [ standard-library ];
+
+  meta = with lib; {
+    inherit (src.meta) homepage;
+    description = "A new Categories library";
+    license = licenses.bsd3;
+    platforms = platforms.unix;
+    hydraPlatforms = [];
+    maintainers = with maintainers; [ alexarice ];
+  };
+}
