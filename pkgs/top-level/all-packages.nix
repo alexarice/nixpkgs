8f832060fe5cb26219ed3aaad2f31c35f9e755bf
Alex Rice <alexrice999@hotmail.co.uk>
2019-12-29 12:11:49 +0000

    Agda-Sheaves: remove broken package

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -14738,8 +14738,6 @@ in
     inherit (haskellPackages) ghcWithPackages;
   };
 
-  AgdaSheaves = callPackage ../development/libraries/agda/Agda-Sheaves { };
-
   bitvector = callPackage ../development/libraries/agda/bitvector { };
 
   categories = callPackage ../development/libraries/agda/categories { };
